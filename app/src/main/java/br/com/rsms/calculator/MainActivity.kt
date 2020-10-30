0726ddc603581e6e4846a5bf345d361b439e9237
Rodrigo Santos Menezes <rodrigo.menezes@cnj.jus.br>
2020-10-30 18:47:11 -0300

    Adding mXparser for expression evaluation

--- app/src/main/java/br/com/rsms/calculator/MainActivity.kt
+++ app/src/main/java/br/com/rsms/calculator/MainActivity.kt
@@ -3,14 +3,42 @@ package br.com.rsms.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import com.google.android.material.button.MaterialButton
+import org.mariuszgromada.math.mxparser.Expression
+import org.mariuszgromada.math.mxparser.mXparser
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun numberInput(view : View){
-        println("haksdjaksd")
+        var viewCopy : MaterialButton = view as MaterialButton
+        this.addToDisplay(viewCopy.text.toString())
+    }
+
+    fun addToDisplay(str : String){
+        var display : EditText = findViewById(R.id.inpExpression)
+        display.setText(display.text.toString() + str)
+    }
+
+    fun clear(view : View) {
+        this.getDisplay().setText("")
+    }
+
+    fun eval(view : View){
+        val displayExpression: String = this.getDisplay().text.toString()
+        val expression: Expression = Expression(displayExpression)
+        this.getDisplay().setText(displayExpression + " = " + expression.calculate())
+    }
+
+    private fun getDisplay() : EditText {
+        val display : EditText = findViewById(R.id.inpExpression)
+        return display
     }
 }
